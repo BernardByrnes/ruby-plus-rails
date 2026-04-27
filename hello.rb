61e4d42522a7ff7b6720b9d85ef777cb09d835ad
BernardByrnes <bernardtamboedu@gmail.com>
2026-04-27 18:10:43 +0300

    Auto-commit: 55 lines changed (added 54, deleted 1)

--- hello.rb
+++ hello.rb
@@ -73,3 +73,56 @@ puts greeting + ", " + name
 puts "#{greeting}, #{name}!"
 
 puts '#{greeting}, #{name}'
+
+puts "bernard".upcase
+puts "BERNARD".downcase
+puts "bernard".capitalize
+puts "bernard".reverse
+puts "bernard".length
+puts "bernard".include?("nar")
+puts " ben ".strip
+puts "a,b,c".split(",")
+puts "bernard".chars
+
+age = 25           # Integer
+price = 9.99       # Float
+
+# Math works as expected
+puts 10 + 3        # => 13
+puts 10 - 3        # => 7
+puts 10 * 3        # => 30
+puts 10 / 3        # => 3   ⚠️ INTEGER division (like Python 2 / Java)
+puts 10.0 / 3      # => 3.333...
+puts 10 % 3        # => 1   (modulo)
+puts 10 ** 3       # => 1000 (exponent — like Python, not JS!)
+
+
+5.even?
+6.even?
+7.odd?
+(-5).abs
+3.14.round
+3.64.round
+3.14.round(1)
+4.7.ceil
+4.7.floor
+5.times{puts "hello Bro"}
+
+if 0
+  puts "Zero is truthy in Ruby!"   # ✅ This prints
+end
+
+if ""
+  puts "Empty string is truthy too!"   # ✅ This also prints
+end
+
+
+"42".to_i
+"3.14".to_f
+48.to_s
+nil.to_s
+[1,2,3].to_s
+
+puts "What's your name?"
+name = gets.chomp
+puts "Hello, #{name}!"
